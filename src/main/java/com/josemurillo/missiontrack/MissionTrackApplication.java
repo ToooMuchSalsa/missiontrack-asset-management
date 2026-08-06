b13ef38cc36cda504d887b51bec9bd39b8b2031c
Jose Murillo <jojo.fb@live.com>
2026-08-06 15:32:27 -0700

    Initialize MissionTrack Spring Boot application

---
+++ src/main/java/com/josemurillo/missiontrack/MissionTrackApplication.java
@@ -0,0 +1,13 @@
+package com.josemurillo.missiontrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissionTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissionTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josemurillo/missiontrack/MissionTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josemurillo.missiontrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
